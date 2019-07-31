3717ada209589bc6d88503f4c40f2f25247ae36a
Teslothorcha <juandavidmarinbernal@gamil.com>
2019-07-31 03:50:49 +0000

    [Feature] #3 These are all the function that printf will use as auxiliars

---
+++ conv_functions.c
@@ -0,0 +1,68 @@
+#include "holberton.h"
+/**
+ * _strlen - gives string lenght.
+ *
+ * @s: char input
+ * Return: lenght of input
+ */
+int _strlen(const char *s)
+{
+	int le = 0;
+
+	while (s[le] != '\0')
+	{
+		le++;
+	}
+	return (le);
+}
+#include "holberton.h"
+/**
+ * cha_r - prints char type on stdoutput
+ *
+ * @list: variadic arguments
+ * Return: amount of char printed
+ */
+int cha_r(va_list list)
+{
+	char ch;
+
+	ch = va_arg(list, int);
+	if (ch == 0)
+		return (0);
+	_putchar(ch);
+	return (1);
+}
+#include "holberton.h"
+/**
+ * strin_g - Prints strings on stdoutput
+ *
+ * @list: Variadic arguments
+ * Return: Amount of chars printed
+ */
+int strin_g(va_list list)
+{
+
+	char *str;
+	unsigned int i, len;
+
+	str = va_arg(list, char *);
+	if (str == NULL || ((char *)0))
+		str = "(null)";
+	len = _strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	return (len);
+}
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
